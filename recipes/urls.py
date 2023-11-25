b647d43c85166ac28dba4b4772a3026049885964
Antonio Jorge <antonio.acjorge@gmail.com>
2023-11-24 21:22:30 -0300

    Mudanças no Admin

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from recipes.views import home
+from . import views
+
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', home),  # Home
-
+    path('', views.home, name="home"),  # Home
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
